f1fe8a7e70e59de5e697e8458ee384daccd21011
Aaradhy Raghav Duvey <you@example.com>
2025-07-06 18:34:34 +0530

    Add initial Flask application with info and phone routes

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/info')
+def info():
+    return "This is the info page."
+
+@app.route('/phone') 
+def phone():
+    return "This is the phone page."
+
+app.run(host='0.0.0.0', debug=True)
