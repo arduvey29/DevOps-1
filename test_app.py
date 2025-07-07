add09387fc0aef56fb27e14e8d81de3169b1a2ee
Aaradhy Raghav Duvey <you@example.com>
2025-07-07 08:20:30 +0530

    Update test_info to match the expected response message

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import info, phone
 import pytest
 
 def test_info():
-    assert info() == "This is the info page."
+    assert info() == "Heyy!! This is the info page."
 
 def test_phone():
     assert phone() == "This is the phone page."
